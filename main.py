1805fa70b39994b6df14d0f037d46b735fb7c6ad
tkonrad <tobias.k48@gso.schule.koeln>
2025-01-16 13:46:22 +0100

    feat: create endpoint for api

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/tables")
+def get_free_tables():
+    return "GET on /tables is working"
+
+@app.post("reservations")
+def book_table():
+    return "POST on /reservations is working"
+
+@app.delete("/reservations")
+def cancel_reservation():
+    return "DELETE on /reservations is working"
+
+@app.get("/reservations")
+def get_all_reservations():
+    return "GET on /reservations is working"
