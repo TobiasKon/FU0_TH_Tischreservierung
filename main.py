a80c9712207e477a305a24ce09343794fdb3fb61
tkonrad <tobias.k48@gso.schule.koeln>
2025-01-23 12:05:43 +0100

    feat: remove unsused code

--- main.py
+++ main.py
@@ -36,47 +36,11 @@ app = FastAPI()
 def on_startup():
     create_db_and_tables()
 
-
-@app.post("/heroes/")
-def create_hero(hero: Hero, session: SessionDep) -> Hero:
-    session.add(hero)
-    session.commit()
-    session.refresh(hero)
-    return hero
-
-
-@app.get("/heroes/")
-def read_heroes(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Hero]:
-    heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-    return heroes
-
-
-@app.get("/heroes/{hero_id}")
-def read_hero(hero_id: int, session: SessionDep) -> Hero:
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    return hero
-
-
-@app.delete("/heroes/{hero_id}")
-def delete_hero(hero_id: int, session: SessionDep):
-    hero = session.get(Hero, hero_id)
-    if not hero:
-        raise HTTPException(status_code=404, detail="Hero not found")
-    session.delete(hero)
-    session.commit()
-    return {"ok": True}
-
 @app.get("/tables")
 def get_free_tables():
     return "GET on /tables is working"
 
-@app.post("reservations")
+@app.post("/reservations")
 def book_table():
     return "POST on /reservations is working"
 
